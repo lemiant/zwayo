bc5f20675c03f5a8afb039aa3989b41310e41a95
Alex Rodrigues <lemiant@gmail.com>
2014-02-22 12:11:21 -0500

    Actual start

---
+++ mysql_utils.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 2/22/14
+ * Time: 12:01 PM
+ */
+
+function connect_to_mch(){
+    $con = mysqli_connect("localhost","root","","mch");
+}
+
+?>

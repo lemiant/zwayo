29d2eff971044eb625888e64c2fddd6e5425d0fd
Kunal Khamar <kkhamar@brontecollege.ca>
2014-02-22 18:06:45 -0500

    reorganized folder

---
+++ server/get_queue_actions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 2/22/14
+ * Time: 2:37 PM
+ */
+require_once("mysql_utils.php");
+$con = connect_to_mch();
+
+if(!empty($_POST['party_id'])){
+    $party_id = mysqli_real_escape_string($con, $_COOKIE['party_id']);
+    if(isset($_POST['last'])) $last = mysqli_real_escape_string($con, $_POST['last']);
+    else $last = 0;
+
+    $query = "SELECT id, action, body FROM queue_actions WHERE `party_id`=$party_id AND `id` > $last";
+    $result = mysqli_query($con, $query);
+    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    //Only set_active once
+    $output = array();
+    $s_a = False;
+    foreach($rows as $row){
+        if($row['action'] == 'set_active') $s_a = $row;
+        else $output[] = $row;
+    }
+    if($s_a) $output[] = $s_a;
+
+    print json_encode(array('result' => 'success', 'items' => $output));
+}
+else{
+    die($FAILURE);
+}
+?>
---
+++ server/mysql_utils.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 2/22/14
+ * Time: 12:01 PM
+ */
+
+function connect_to_mch(){
+    return mysqli_connect("localhost","root","","mch");
+}
+function check_party($con, $party_id){
+    $query = "SELECT id FROM parties WHERE id='$party_id'";
+    $result = mysqli_query($con, $query);
+    if(mysqli_fetch_row($result)){
+        return true;
+    }
+    else return false;
+}
+function check_admin_key($con, $party_id, $admin_key){
+    $query = "SELECT admin_key FROM parties WHERE id=$party_id";
+    $result= mysqli_query($con, $query);
+
+    if($row= mysqli_fetch_row($result,MYSQLI_ASSOC)){
+        if ($row["admin_key"]== $admin_key){
+            return true;
+        }
+    }
+    return false;
+}
+function rand_letter($num=1){
+    $key = "";
+    for ($i=0; $i<$num; $i++)
+	{
+        $key .= chr(97+ mt_rand(0,25));
+    }
+	return $key;
+}
+
+$FAILLURE = "dd";
+?>

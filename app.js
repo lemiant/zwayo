aedc9b50716b94735a0ff55c87ef7b9a1a6d901c
Alex Rodrigues <lemiant@gmail.com>
2014-04-19 14:16:37 -0400

    Live synchronization demo!

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var api_router = require('zw_api/router')
+var api_ws = require('zw_api/ws')
+var socketio = require('socket.io')
 
 // Mongo Stuff ///////////////////////
 var mongo = require('mongodb');
@@ -19,16 +21,16 @@ var db = monk('localhost:27017/nodetest');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
+//app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
+//app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -41,10 +43,14 @@ app.get('/list', routes.list(db));
 app.get('/api/*', function(q,r,n){ api_router(q,r,n,db) });
 app.get('*', routes.static_views);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app),
+    io = socketio.listen(server)
+
+api_ws(io)
+
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
 
 
 
---
+++ node_bootstrap.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Get root up in here
+sudo su
+
+# Just a simple way of checking if we need to install everything
+if [ ! -d "/var/www" ]
+then
+    # Add MongoDB to apt
+    apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
+
+    # Update and begin installing some utility tools
+    apt-get -y update
+    apt-get install -y python-software-properties
+    apt-get install -y vim git subversion curl
+    apt-get install -y memcached build-essential
+
+    # Add nodejs repo
+    add-apt-repository -y ppa:chris-lea/node.js
+    apt-get -y update
+
+    # Install nodejs
+    apt-get install -y nodejs
+    npm install supervisor -g
+
+    # Install latest stable version of MongoDB
+    apt-get install -y mongodb-10gen
+
+    # Symlink our host www to the guest /var/www folder
+    ln -s /vagrant /var/www
+
+    # Victory!
+    echo "You're all done! Your default node server should now be listening on http://10.0.43.34/. For code, see: node-mongo-vagrant/www/default/server.js."
+
+    # Run it
+    supervisor /var/www/app.js
+fi
